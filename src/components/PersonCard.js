6ea7cf857218efdbfa90e2108dee1c4a72183d31
Mortron <cmsl4y3r@gmail.com>
2021-12-22 20:56:04 -0700

    complete

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,16 +1,27 @@
-import React, { component } from 'react';
+import React, { Component } from 'react';
 
 class PersonCard extends React.Component {
-
+    constructor() {
+        super();
+        this.state = {
+            newage: 0
+        };
+    }
     
+    ageUp (age) {
+        this.setState.newAge(age);
+        this.setState({ newAge: this.state.newage +1 })
+    }
+
     render(props) {
         const {firstName, lastName, age, hairColor } = this.props;
-
+        
+        
 
         return(
             <div>
-                <h1>{lastName}, {firstName} </h1>
-                <h3>Age: {age} </h3>
+                <h1>{lastName}, {firstName} {this.state.newage} </h1>
+                <h3>Age:  <button onClick={(age) => this.setState({ age: this.state.newage += 1})}>Click{this.state.age}</button>  </h3>
                 <h3>Hair Color : {hairColor} </h3>
             </div>
             
