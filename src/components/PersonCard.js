8ba21027b59a5ef35458b31d13cfc04ace3ba836
Mortron <cmsl4y3r@gmail.com>
2021-12-22 19:54:05 -0700

    finished big inversion assignment

---
+++ src/components/PersonCard.js
@@ -0,0 +1,23 @@
+import React, { component } from 'react';
+
+class PersonCard extends React.Component {
+
+    
+    render(props) {
+        const {firstName, lastName, age, hairColor } = this.props;
+
+
+        return(
+            <div>
+                <h1>{lastName}, {firstName} </h1>
+                <h3>Age: {age} </h3>
+                <h3>Hair Color : {hairColor} </h3>
+            </div>
+            
+
+        );
+    }
+
+}
+
+export default PersonCard;
